727735cc6ec95559e38d20b41d4a090cdef145fc
Mathias Klenk <mathias.j.klenk@gmail.com>
2024-06-19 13:14:24 +0200

    Turned handbook into list

--- src/components/handbook.tsx
+++ src/components/handbook.tsx
@@ -4,54 +4,62 @@ export function Handbook() {
   return (
     <div className="space-y-4">
       <p className="pt-6 font-bold">Background</p>
-      <p>
-        Built an identity system at{" "}
-        <a
-          className="font-bold underline"
-          target="_blank"
-          href="https://passbase.com"
-        >
-          Passbase
-        </a>
-      </p>
-      <p>
-        Currently building an HR operating system for frontline workers at{" "}
-        <a
-          className="font-bold underline"
-          target="_blank"
-          href="https://midlane.com"
-        >
-          Midlane
-        </a>
-      </p>
-      <p>Loves open-source</p>
+      <ul className="list-disc space-y-2 px-5">
+        <li>
+          Built an identity system at{" "}
+          <a
+            className="font-bold underline"
+            target="_blank"
+            href="https://passbase.com"
+          >
+            Passbase
+          </a>
+        </li>
+        <li>
+          Currently building an HR operating system for frontline workers at{" "}
+          <a
+            className="font-bold underline"
+            target="_blank"
+            href="https://midlane.com"
+          >
+            Midlane
+          </a>
+        </li>
+        <li>Loves open-source</li>
+      </ul>
 
       <p className="pt-6 font-bold">Values, Principles, Beliefs</p>
-      <p>We solve the hard problems and make our customers the heros</p>
-      <p>Engineering velocity is the most important metric to optimize</p>
-      <p>I care a lot about UI & UX</p>
-      <p>Done means code that runs in production and is used by customers</p>
-      <p>Processes kills most innovation</p>
-      <p>
-        It&apos;s better to suffer a short period a lot, than a long time a
-        little bit
-      </p>
-      <p>If you truly care about a problem you can&apos;t sleep at night</p>
-      <p>The best people want to play in a Championship team</p>
+      <ul className="list-disc space-y-2 px-5">
+        <li>We solve the hard problems and make our customers the heros</li>
+        <li>Engineering velocity is the most important metric to optimize</li>
+        <li>I care a lot about UI & UX</li>
+        <li>
+          Done means code that runs in production and is used by customers
+        </li>
+        <li>Processes kills most innovation</li>
+        <li>
+          It&apos;s better to suffer a short period a lot, than a long time a
+          little bit
+        </li>
+        <li>If you truly care about a problem you can&apos;t sleep at night</li>
+        <li>The best people want to play in a Championship team</li>
+      </ul>
 
       <p className="pt-6 font-bold">Work Style</p>
-      <p>
-        I love ideating and innovating. I hate making the operational plan for
-        it
-      </p>
-      <p>MVP first, then iteration 1 to n to shape it</p>
-      <p>
-        I am driven by bursts of energy. Sometimes I don&apos;t get a lot of
-        things done, but then a lot in a short amount of time
-      </p>
-      <p>If it is not written down, it&apos;s not decided</p>
-      <p>Don&apos;t bring me the problem, but the solution</p>
-      <p>Don&apos;t ask for permission, but for forgiveness</p>
+      <ul className="list-disc space-y-2 px-5">
+        <li>
+          I love ideating and innovating. I hate making the operational plan for
+          it
+        </li>
+        <li>MVP first, then iteration 1 to n to shape it</li>
+        <li>
+          I am driven by bursts of energy. Sometimes I don&apos;t get a lot of
+          things done, but then a lot in a short amount of time
+        </li>
+        <li>If it is not written down, it&apos;s not decided</li>
+        <li>Don&apos;t bring me the problem, but the solution</li>
+        <li>Don&apos;t ask for permission, but for forgiveness</li>
+      </ul>
     </div>
   )
 }
