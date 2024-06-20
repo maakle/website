780d7b2d45e9066c610a35c9cdd888e868d64ef7
Mathias Klenk <mathias.j.klenk@gmail.com>
2024-06-20 11:22:42 +0200

    Added third parties and Google tag

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,7 +5,7 @@ import { env } from "@/env.mjs"
 export const siteConfig: SiteConfig = {
   name: "maakle",
   author: "maakle",
-  description: "My personal website built in Nextjs.",
+  description: "Maakle's website",
   url: env.NEXT_PUBLIC_APP_URL,
   ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
   links: {
