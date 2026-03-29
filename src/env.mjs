b4769b7c5fa1e21f9e75385b55e298ff54e75b0f
Mathias Klenk <mathias.j.klenk@gmail.com>
2026-03-29 19:21:38 +0200

    feat: add Strava environment variables

--- src/env.mjs
+++ src/env.mjs
@@ -4,12 +4,18 @@ import { z } from "zod"
 export const env = createEnv({
   server: {
     GH_API_URL: z.string().startsWith("https://"),
+    STRAVA_CLIENT_ID: z.string().min(1),
+    STRAVA_CLIENT_SECRET: z.string().min(1),
+    STRAVA_REFRESH_TOKEN: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
     GH_API_URL: process.env.GH_API_URL,
+    STRAVA_CLIENT_ID: process.env.STRAVA_CLIENT_ID,
+    STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
+    STRAVA_REFRESH_TOKEN: process.env.STRAVA_REFRESH_TOKEN,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })
