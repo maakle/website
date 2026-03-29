90d91e0ef0829d5d397c0dbb56b587a0ff4fc005
Mathias Klenk <mathias.j.klenk@gmail.com>
2026-03-29 19:27:59 +0200

    feat: update statistics skeleton for Strava sections

--- src/components/loaders/statistics-skeleton.tsx
+++ src/components/loaders/statistics-skeleton.tsx
@@ -1,52 +1,29 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card"
 import { Skeleton } from "@/components/ui/skeleton"
 
 export function StatisticSkeleton() {
   return (
-    <>
-      <div className="grid w-full grid-cols-1 gap-2 md:grid-cols-2">
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Coding hours</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <Skeleton className="h-8 w-[12.5rem]" />
-            <Skeleton className="h-4 w-[8rem]" />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Most used language
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <Skeleton className="h-8 w-[12.5rem]" />
-            <Skeleton className="h-4 w-[8rem]" />
-          </CardContent>
-        </Card>
-      </div>
-      <Card className="w-full pt-6">
-        <CardContent className="space-y-2">
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
+    <div className="flex flex-col gap-4">
+      <Card className="w-full p-3">
+        <CardContent className="space-y-2 p-0">
+          <Skeleton className="h-[150px] w-full" />
         </CardContent>
       </Card>
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle className="text-lg">Top Languages</CardTitle>
-        </CardHeader>
-        <CardContent className="grid grid-cols-2 md:grid-cols-4">
-          {Array.from({ length: 16 }).map((_, index) => (
-            <div className="space-y-2 p-3" key={index}>
-              <Skeleton className="h-4 w-[6rem]" />
-              <Skeleton className="h-4 w-[5rem]" />
-            </div>
-          ))}
+      <Card className="w-full p-3">
+        <CardContent className="space-y-2 p-0">
+          <Skeleton className="h-[150px] w-full" />
         </CardContent>
       </Card>
-    </>
+      <Card className="w-full p-3">
+        <CardContent className="space-y-2 p-0">
+          <Skeleton className="h-[200px] w-full" />
+        </CardContent>
+      </Card>
+      <Card className="w-full p-3">
+        <CardContent className="space-y-2 p-0">
+          <Skeleton className="h-[180px] w-full" />
+        </CardContent>
+      </Card>
+    </div>
   )
 }
